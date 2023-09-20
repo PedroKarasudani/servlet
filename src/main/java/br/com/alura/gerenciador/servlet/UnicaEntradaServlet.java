e1bbbb917d6669dec1a2e69a795bcea0fd66868d
Pedro K. L <pedro.karasudani@hotmail.com>
2023-09-20 10:35:10 -0300

    Escondendo JSP, só chama pelo controlador

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.acao.AlteraEmpresa;
+import br.com.alura.gerenciador.acao.FormNovaEmpresa;
 import br.com.alura.gerenciador.acao.ListaEmpresas;
 import br.com.alura.gerenciador.acao.MostraEmpresa;
 import br.com.alura.gerenciador.acao.NovaEmpresa;
@@ -47,13 +48,17 @@ public class UnicaEntradaServlet extends HttpServlet {
 		    } else if(paramAcao.equals("NovaEmpresa")) {
 		    	NovaEmpresa acao = new NovaEmpresa();
 		    	nome =  acao.executa(request, response);
+		    	
+		    } else if(paramAcao.equals("FormNovaEmpresa")) {
+		    	FormNovaEmpresa acao = new FormNovaEmpresa();
+		    	nome =  acao.executa(request, response);
 		    }
 		    
 		    
 		    String[] tipoEEndereco = nome.split(":");
 		    
 		    if (tipoEEndereco[0].equals("forward")) {
-			    RequestDispatcher rd = request.getRequestDispatcher(tipoEEndereco[1]);
+			    RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view"+ tipoEEndereco[1]);
 				rd.forward(request, response);
 		    } else if (tipoEEndereco[0].equals("redirect")) {
 		    	response.sendRedirect(tipoEEndereco[1]);
