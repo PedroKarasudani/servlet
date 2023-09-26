cd444c4292632cbbc72ab25566e54ddeba956636
Pedro K. L <pedro.karasudani@hotmail.com>
2023-09-26 20:26:03 -0300

    primeiro filtro, tempo execucao (implements Filter)

--- src/main/java/br/com/alura/gerenciador/acao/Logout.java
+++ src/main/java/br/com/alura/gerenciador/acao/Logout.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import br.com.alura.gerenciador.modelo.Banco;
-import br.com.alura.gerenciador.modelo.Usuario;
-
 public class Logout implements Acao {
 
 	@Override
--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+ package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
 
