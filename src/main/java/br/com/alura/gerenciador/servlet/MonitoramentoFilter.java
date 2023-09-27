ca39f90bb6f0e625db004dc930b4c3e87efe8cb2
Pedro K. L <pedro.karasudani@hotmail.com>
2023-09-26 21:46:43 -0300

    Ordem de execucao

--- src/main/java/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
+++ src/main/java/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
@@ -7,10 +7,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 
 
-@WebFilter("/entrada")
+//@WebFilter("/entrada")
 public class MonitoramentoFilter implements Filter {
 
 	@Override
